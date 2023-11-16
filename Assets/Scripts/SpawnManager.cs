ac1cba0f098421770171086cb9a05ef598e5b457
Muppetsg2 <muppetsg2@gmail.com>
2023-11-16 01:26:57 +0100

    Spawn Manager Loop

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
 
 [System.Serializable]
 public class SpawnPrefab
@@ -24,25 +25,56 @@ public class SpawnPoint
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] private List<SpawnPoint> spawnList = new();
+    [SerializeField] 
+    private bool loop = false;
+    [SerializeField]
+    [ShowIf("loop")]
+    private float timeBetweenWaves = 5.0f;
+    [SerializeField]
+    private float timeBetweenObjects = 1.0f;
+    [SerializeField] 
+    private List<SpawnPoint> spawnList = new();
+
+    private float timeBefore = 0.0f;
 
     void Awake()
     {
+        timeBefore = timeBetweenWaves + SpawnEverything(timeBefore);
+    }
+
+    private void Update()
+    {
+        if (loop)
+        {
+            timeBefore = timeBetweenWaves + SpawnEverything(timeBefore);
+        }
+    }
+
+    float SpawnEverything(float addT)
+    {
+        float lastTime = 0.0f;
         foreach (SpawnPoint point in spawnList)
         {
-            int time = 0;
+            float time = addT;
 
             foreach (SpawnPrefab prefab in point.GetSpawnPrefabs())
             {
-                for (int i = 0; i < prefab.GetCount(); ++i, ++time)
+                for (int i = 0; i < prefab.GetCount(); ++i, time += timeBetweenObjects)
                 {
                     StartCoroutine(Spawn(prefab.GetPrefab(), point.GetTransform(), time));
                 }
             }
+
+            if (lastTime < time)
+            {
+                lastTime = time;
+            }
         }
+
+        return lastTime;
     }
 
-    IEnumerator Spawn(GameObject prefab, Transform tran, int time)
+    IEnumerator Spawn(GameObject prefab, Transform tran, float time)
     {
         yield return new WaitForSeconds(time);
         Instantiate(prefab, tran.position, Quaternion.identity);
