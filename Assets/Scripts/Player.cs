954b7b88924fcc4237a840145e194065367db45f
MAIPA01 <marcelipatryk@gmail.com>
2023-11-23 00:58:03 +0100

    Indicator Wstęp

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
 using UnityEditor.Animations;
+using UnityEngine.Rendering;
+using System.Linq.Expressions;
 
 enum PlayerForm
 {
@@ -72,6 +74,14 @@ public class Player : ObjectHealth
 
     private bool canAttack = true;
 
+    private Sprite spiritSlashMask = null;
+    [SerializeField]
+    private Sprite spiritSlashSprite = null;
+    [SerializeField]
+    private int slashMaskSpriteResolution = 64;
+    [SerializeField]
+    private Vector2 slashMaskPivot = Vector2.zero;
+
     private void OnValidate()
     {
         if (slashObject == null)
@@ -88,6 +98,11 @@ public class Player : ObjectHealth
         {
             body.color = IsSpirit() ? spiritColor : bloodColor;
         }
+
+        if (spiritSlashMask == null)
+        {
+            ReloadSpiritSlashMaskSprite();
+        }
     }
 
     void Start()
@@ -97,6 +112,8 @@ public class Player : ObjectHealth
         {
             Debug.LogError("Could not find Game Manager! Paste prefab on scene please <3");
         }
+
+        CreateSlashMaskSprite();
     }
 
     void Update()
@@ -201,6 +218,84 @@ public class Player : ObjectHealth
         ChangeForm();
     }
 
+    [Button]
+    private void ReloadSpiritSlashMaskSprite()
+    {
+        Texture2D texture2D = new(slashMaskSpriteResolution, slashMaskSpriteResolution);
+        for (int y = 0; y < texture2D.height; y++)
+        {
+            for (int x = 0; x < texture2D.width; x++)
+            {
+                texture2D.SetPixel(x, y, Color.white);
+            }
+        }
+        texture2D.Apply();
+        spiritSlashMask = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), Vector2.zero, 64);
+    }
+
+    private void CreateSlashMaskSprite()
+    {
+        if (spiritSlashMask != null)
+        {
+            List<Vector3> meshVerticies = new();
+            List<int> meshTriangles = new();
+
+            // CENTER POINT
+            Vector3 origin = new(0f, 0f, 0f);
+            meshVerticies.Add(origin);
+
+            float halfSectorRadians = (sectorAngle / 2f) * Mathf.Deg2Rad;
+            float startRadians = halfSectorRadians * 2f;
+            float endRadians = 0f;
+
+            Vector3 startPoint = (new Vector2(Mathf.Cos(startRadians), Mathf.Sin(startRadians))).normalized;
+            Vector3 endPoint = (new Vector2(Mathf.Cos(endRadians), Mathf.Sin(endRadians))).normalized;
+
+            // TOP POINT
+            meshVerticies.Add(origin + startPoint * circleRadius);
+
+            uint pointsNum = (uint)Mathf.Abs((int)((endRadians - startRadians) / (Mathf.Deg2Rad * 3f)));
+            float radiansDiff = (endRadians - startRadians) / pointsNum;
+            for (int i = 1; i < pointsNum - 1; i++)
+            {
+                // POINTS BETWEEN TOP AND BOTTOM
+                Vector3 point = (new Vector3(Mathf.Cos(startRadians + i * radiansDiff), Mathf.Sin(startRadians + i * radiansDiff), 0f)).normalized;
+
+                meshVerticies.Add(origin + point * circleRadius);
+            }
+            // BOTTOM POINT
+            meshVerticies.Add(origin + endPoint * circleRadius);
+
+            for (int i = 1; i < meshVerticies.Count - 1; i++)
+            {
+                meshTriangles.Add(0);
+                meshTriangles.Add(i);
+                meshTriangles.Add(i + 1);
+            }
+
+            MeshToSprite.ConvertMeshsToSprite(meshVerticies.ToArray(), meshTriangles.ToArray(), ref spiritSlashMask);
+
+            if (sectorAngle > 90f)
+            {
+                float alpha = (sectorAngle - 90f) * Mathf.Deg2Rad;
+                slashMaskPivot = new Vector2(Mathf.Sin(alpha) / (Mathf.Sin(alpha) + Mathf.Cos(alpha)), 0f);
+            }
+            else if (sectorAngle > 180f)
+            {
+                float alpha = (sectorAngle - 180f) * Mathf.Deg2Rad;
+                slashMaskPivot = new Vector2(.5f, Mathf.Sin(alpha) / 2f);
+            }
+            else if (sectorAngle > 270f)
+            {
+                slashMaskPivot = new Vector2(.5f, .5f);
+            }
+            else
+            {
+                slashMaskPivot = Vector2.zero;
+            }
+        }
+    }
+
     private void SpiritAttack()
     {
         // Dodaæ Delay
@@ -224,6 +319,28 @@ public class Player : ObjectHealth
                 spiritEnemy.GetComponent<Rigidbody2D>().velocity += throwBackDir * spiritDamage;
             }
         }
+
+        // całe to rysowanie działa na razie dla kątów mniejszych równych 90 stopni
+        GameObject slash = new("Slash");
+        slash.transform.position = transform.position;
+        float max = slashMaskPivot.x > slashMaskPivot.y ? slashMaskPivot.x : slashMaskPivot.y;
+        float radius = circleRadius / (2f * (1f - max));
+        slash.transform.localScale = new Vector3(1f, 1f) * radius + Vector3.forward;
+        slash.transform.parent = this.transform;
+        slash.AddComponent<SpriteMask>().sprite = spiritSlashMask;
+
+        GameObject slashSprite = new("SlashSprite");
+        slashSprite.transform.parent = slash.transform;
+        slashSprite.transform.position = transform.position;
+        slashSprite.transform.localScale = new Vector3(1f, 1f) * 2f * radius * 0.64f + Vector3.forward; // NIE WIEM SKĄD 0.64 nie mam siły teraz tego liczyć i sprawdzać
+        SpriteRenderer renderer = slashSprite.AddComponent<SpriteRenderer>();
+        renderer.sortingOrder = 1;
+        renderer.sprite = spiritSlashSprite;
+        renderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+
+        slash.transform.Rotate(Vector3.forward, Vector2Extensions.Angle360(Vector2.right, lookDir) - sectorAngle / 2f);
+        slash.transform.Translate(-slashMaskPivot * radius); // Wstęp do wyższych kątów (na razie nie działa)
+        Destroy(slash, .5f);
     }
 
     private void OnDrawGizmos()
@@ -250,14 +367,15 @@ public class Player : ObjectHealth
             Gizmos.DrawLine(origin, origin + endPoint * circleRadius);
 
             float radiansDiff = (endRadians - startRadians) / 10;
-            for (int i = 1; i < 11; i++)
+            Vector2 point1 = startPoint;
+            for (int i = 0; i < 10 - 1; i++)
             {
-                Vector2 point1 = (new Vector2(Mathf.Cos(startRadians + (i - 1) * radiansDiff), Mathf.Sin(startRadians + (i - 1) * radiansDiff))).normalized;
-                Vector2 point2 = (new Vector2(Mathf.Cos(startRadians + i * radiansDiff), Mathf.Sin(startRadians + i * radiansDiff))).normalized;
+                Vector2 point2 = (new Vector2(Mathf.Cos(startRadians + (i + 1) * radiansDiff), Mathf.Sin(startRadians + (i + 1) * radiansDiff))).normalized;
 
-                Gizmos.color = Color.red;
                 Gizmos.DrawLine(origin + point1 * circleRadius, origin + point2 * circleRadius);
+                point1 = point2;
             }
+            Gizmos.DrawLine(origin + point1 * circleRadius, origin + endPoint * circleRadius);
         }
         else
         {
