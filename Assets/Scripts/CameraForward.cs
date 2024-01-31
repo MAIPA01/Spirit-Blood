f558cc1ce08ddae67c9403766db69ddafb1d6236
MAIPA01 <marcelipatryk@gmail.com>
2024-01-31 18:59:18 +0100

    Some Fixes And Addons

--- Assets/Scripts/CameraForward.cs
+++ Assets/Scripts/CameraForward.cs
@@ -1,14 +1,23 @@
+using NaughtyAttributes;
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class CameraForward : MonoBehaviour
 {
     public Camera cam;
+    [Header("Camera Forward Follow")]
     //public Rigidbody2D target; // 2D
     public Rigidbody target;
     public Vector3 offset;
     public float forwardDistance = 5f;
     public float changeDirSpeed = 1.8f;
+
+    // test
+    [Header("Edge Snapping")]
+    public bool enableSnapping = true;
+    [ShowIf("enableSnapping")]
+    public float minX, maxX, minY, maxY;
 
     private void OnValidate()
     {
@@ -27,6 +36,17 @@ public class CameraForward : MonoBehaviour
             return;
 
         ForwardFollow();
+
+        if (enableSnapping)
+        {
+            float halfHeight = cam.orthographicSize;
+            float halfWidth = cam.aspect * halfHeight;
+
+            Vector2 transform = Vector2.zero;
+            transform.x = Mathf.Min(Mathf.Max(cam.transform.position.x, minX + halfWidth), maxX - halfWidth);
+            transform.y = Mathf.Min(Mathf.Max(cam.transform.position.y, minY + halfHeight), maxY - halfHeight);
+            cam.transform.position = new Vector3(transform.x, transform.y, cam.transform.position.z);
+        }
     }
 
     void ForwardFollow()
@@ -44,5 +64,28 @@ public class CameraForward : MonoBehaviour
         float x = Mathf.Lerp(cam.transform.position.x, targetPos.x, Time.deltaTime * changeDirSpeed);
         float y = Mathf.Lerp(cam.transform.position.y, targetPos.y, Time.deltaTime * changeDirSpeed);
         cam.transform.position = new Vector3(x, y, cam.transform.position.z);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (enableSnapping)
+        {
+            Gizmos.color = Color.yellow;
+
+            Gizmos.DrawLine(new Vector3(minX, minY, cam.transform.position.z), new Vector3(minX, maxY, cam.transform.position.z));
+            Gizmos.DrawLine(new Vector3(maxX, minY, cam.transform.position.z), new Vector3(maxX, maxY, cam.transform.position.z));
+            Gizmos.DrawLine(new Vector3(minX, minY, cam.transform.position.z), new Vector3(maxX, minY, cam.transform.position.z));
+            Gizmos.DrawLine(new Vector3(minX, maxY, cam.transform.position.z), new Vector3(maxX, maxY, cam.transform.position.z));
+
+            Gizmos.color = Color.red;
+
+            float halfHeight = cam.orthographicSize;
+            float halfWidth = cam.aspect * halfHeight;
+
+            Gizmos.DrawLine(new Vector3(minX + halfWidth, minY + halfHeight, cam.transform.position.z), new Vector3(minX + halfWidth, maxY - halfHeight, cam.transform.position.z));
+            Gizmos.DrawLine(new Vector3(maxX - halfWidth, minY + halfHeight, cam.transform.position.z), new Vector3(maxX - halfWidth, maxY - halfHeight, cam.transform.position.z));
+            Gizmos.DrawLine(new Vector3(minX + halfWidth, minY + halfHeight, cam.transform.position.z), new Vector3(maxX - halfWidth, minY + halfHeight, cam.transform.position.z));
+            Gizmos.DrawLine(new Vector3(minX + halfWidth, maxY - halfHeight, cam.transform.position.z), new Vector3(maxX - halfWidth, maxY - halfHeight, cam.transform.position.z));
+        }
     }
 }
